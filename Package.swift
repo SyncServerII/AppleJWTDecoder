508a3be4f644a2baa769bcdac73386b5f40687f2
Christopher Prince <chris@SpasticMuffin.biz>
2021-07-31 20:51:23 -0600

    version 0.0.7

--- Package.swift
+++ Package.swift
@@ -12,7 +12,9 @@ let package = Package(
             targets: ["AppleJWTDecoder"]),
     ],
     dependencies: [
-        .package(name: "SwiftJWT", url: "https://github.com/IBM-Swift/Swift-JWT.git", from: "3.5.3"),
+        // I'm using my fork so I don't have conflicting versions.
+        .package(name: "SwiftJWT", url: "https://github.com/crspybits/Swift-JWT.git", from: "3.5.3"),
+        
         .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.8.1"),
         .package(name: "SwiftJWKtoPEM", url: "https://github.com/ibm-cloud-security/Swift-JWK-to-PEM.git", from: "0.4.0"),
         .package(name: "Kitura-net", url: "https://github.com/Kitura/Kitura-net.git", from: "2.4.1"),
