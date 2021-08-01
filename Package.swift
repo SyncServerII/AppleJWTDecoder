2cb4d109b2ef8336a87e6a347f77751b1d2d74b3
Christopher Prince <chris@SpasticMuffin.biz>
2021-07-31 21:16:23 -0600

    version 0.0.8

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // I'm using my fork so I don't have conflicting versions.
-        .package(name: "SwiftJWT", url: "https://github.com/crspybits/Swift-JWT.git", from: "3.5.3"),
+        .package(name: "SwiftJWT", url: "https://github.com/IBM-Swift/Swift-JWT.git", from: "3.5.3"),
         
         .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.8.1"),
         .package(name: "SwiftJWKtoPEM", url: "https://github.com/ibm-cloud-security/Swift-JWK-to-PEM.git", from: "0.4.0"),
